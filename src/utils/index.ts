988e10c288b5e911f85ef34f44ef70dff1d18e3b
zhangqixuan <zhangqixuan@qq.com>
2021-08-27 08:46:03 +0800

    fix

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,24 +1,26 @@
 import { useEffect, useState } from "react";
 
-export const isFalsy = (value:any) => (value === 0 ? false : !value);
+export const isFalsy = (value: any) => (value === 0 ? false : !value);
 
-export const cleanObject = (object:object) => {
+export const cleanObject = (object: object) => {
   const result = { ...object };
   Object.keys(result).forEach((key) => {
+    // @ts-ignore
     const value = result[key];
     if (isFalsy(value)) {
+      // @ts-ignore
       delete result[key];
     }
   });
   return result;
 };
-export const useMount = (callback:()=>void) => {
+export const useMount = (callback: () => void) => {
   useEffect(() => {
     callback();
   }, []);
 };
 
-export const useDebounce = (value:any, delay?:number) => {
+export const useDebounce = (value: any, delay?: number) => {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
   useEffect(() => {
